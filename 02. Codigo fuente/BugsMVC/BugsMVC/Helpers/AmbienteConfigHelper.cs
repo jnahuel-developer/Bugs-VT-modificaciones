e22be8898255457097301066c484305561c4f5a3
Nahuel Martinez <jnahuel.developer@gmail.com>
2026-02-18 04:08:29 -0300

    mod0004 - Se agregan keys de ambiente y se registra el modo efectivo al inicio

---
+++ 02. Codigo fuente/BugsMVC/BugsMVC/Helpers/AmbienteConfigHelper.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Configuration;
+
+namespace BugsMVC.Helpers
+{
+    public static class AmbienteConfigHelper
+    {
+        private static readonly log4net.ILog Log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static readonly bool _ambienteDesarrolloHabilitado = ResolverValorAmbiente("Ambiente:Desarrollo");
+        private static readonly bool _ambienteSimuladoresHabilitado = ResolverValorAmbiente("Ambiente:Simuladores");
+
+        public static bool AmbienteDesarrolloHabilitado => _ambienteDesarrolloHabilitado;
+        public static bool AmbienteSimuladoresHabilitado => _ambienteSimuladoresHabilitado;
+
+        public static void Inicializar()
+        {
+            _ = AmbienteDesarrolloHabilitado;
+            _ = AmbienteSimuladoresHabilitado;
+        }
+
+        private static bool ResolverValorAmbiente(string configKey)
+        {
+            string modo = ConfigurationManager.AppSettings[configKey];
+
+            if (string.IsNullOrWhiteSpace(modo))
+            {
+                Log.Info($"No se encontró la configuración '{configKey}'. Se tomará el valor OFF por defecto.");
+                Log.Info($"Se resolvió la configuración '{configKey}' con valor efectivo: OFF.");
+                return false;
+            }
+
+            if (string.Equals(modo, "ON", StringComparison.OrdinalIgnoreCase))
+            {
+                Log.Info($"Se resolvió la configuración '{configKey}' con valor efectivo: ON.");
+                return true;
+            }
+
+            if (string.Equals(modo, "OFF", StringComparison.OrdinalIgnoreCase))
+            {
+                Log.Info($"Se resolvió la configuración '{configKey}' con valor efectivo: OFF.");
+                return false;
+            }
+
+            Log.Info($"Se detectó un valor inválido en '{configKey}' ({modo}). Se tomará el valor OFF por defecto.");
+            Log.Info($"Se resolvió la configuración '{configKey}' con valor efectivo: OFF.");
+            return false;
+        }
+    }
+}
